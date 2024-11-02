bf11f9fff11d135dbd63c083465808c6018e0f4b
Tanmoy-Biswas-2826 <tanmoybiswas2826@gmail.com>
2024-11-03 01:44:10 +0530

    first commit

---
+++ src/main/java/com/example/EcommerceCouponAPi/EcommerceCouponAPiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceCouponAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCouponAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCouponAPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/EcommerceCouponAPi/EcommerceCouponAPiApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.EcommerceCouponAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCouponAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("HEllo World");
+	}
+
+}
